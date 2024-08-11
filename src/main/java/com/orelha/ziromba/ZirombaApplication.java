cf2e017842e189d9138e588d674d4e426031c5ef
micaelsales <micaelpedro846@gmail.com>
2024-08-10 21:09:42 -0300

    commit init

---
+++ src/main/java/com/orelha/ziromba/ZirombaApplication.java
@@ -0,0 +1,13 @@
+package com.orelha.ziromba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZirombaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZirombaApplication.class, args);
+	}
+
+}
